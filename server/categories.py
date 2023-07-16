abd5d3317048660920eb07e80cccc4d898f2ebc3
Aadil Hayat Bhat <aadilhayatbhat184@gmail.com>
2023-07-16 20:07:14 +0530

    added : removed try else code

--- server/categories.py
+++ server/categories.py
@@ -5,33 +5,51 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+from flask import Flask, jsonify
+import mysql.connector
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
 @app.route('/categories', methods=['GET'])
 def search_record():
-    try:
-        connection = mysql.connector.connect(
-            host='63.34.171.72',
-            user='library',
-            password='password',
-            database='library'
-        )
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM categories")
-        records = cursor.fetchall()
+    connection = mysql.connector.connect(
+        host='63.34.171.72',
+        user='library',
+        password='password',
+        database='library'
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM categories")
+    records = cursor.fetchall()
 
-        record_list = []
-        for record in records:
-            record_dict = {'category_id': record[0], 'category_name': record[1]}
-            record_list.append(record_dict)
+    record_list = []
+    for record in records:
+        record_dict = {'category_id': record[0], 'category_name': record[1]}
+        record_list.append(record_dict)
 
-        return jsonify(record_list)
+    cursor.close()
+    connection.close()
 
-    except Exception as e:
-        return jsonify({'message': 'An error occurred', 'error': str(e)})
+    return jsonify(record_list)
 
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
+# #Api to search record by ID 
+# @app.route('/categories/<int:record_id>',methods=['GET'])
+# def search_record(record_id):
+#     try :
+#         connetion = db_connection
+#         cursor = connection.cursor()
+
+
+#         cursor.execute("SELECT * FROM categories WHERE id = %s",(record_id))
+#         record = cursor.fetchone()
+#         cursor.close()
+
+#         if record : 
+
 
 if __name__ == '__main__':
     app.run()
+
+
