478deeeefed62ba4a807040c102da11ff66bb3d5
Madan Srivathsa <madansrivathsa@Madans-MacBook-Air.local>
2023-07-17 10:58:19 +0100

    Added: API for search borrowed books

--- server/borrowed_books.py
+++ server/borrowed_books.py
@@ -43,5 +43,27 @@ def get_all_borrowed_books():
         return jsonify({'message': 'Database connection error'}), 500  # 500 Internal Server Error
 
 
+@app.route('/borrowed_books/<int:record_id>', methods=['GET'])
+def search_record_by_id(record_id):
+    connection = db_connection()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM borrowed_books WHERE book_id = %s", (record_id,))
+            record = cursor.fetchone()
+
+            connection.close()
+
+            if record:
+                record_dict = {'transaction_id': record[0], 'student_id': record[1], 'book_id': record[2], 'borrow_date': record[3], 'due_date': record[4], 'returned_date': record[5]}
+                return jsonify(record_dict), 200  # 200 OK - Successful request
+            else:
+                return jsonify({'message': 'Record not Found'}), 404  # 404 Not Found - Resource not found
+        except mysql.connector.Error as e:
+            print(f"Error fetching data from the database: {e}")
+            return jsonify({'message': 'An error occurred while fetching data'}), 500  # 500 Internal Server Error
+    else:
+        return jsonify({'message': 'Database connection error'}), 500  # 500 Internal Server Error
+
 if __name__ == '__main__':
     app.run()
