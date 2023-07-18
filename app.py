ee52feff47182d284ac5e30ab0d402e5672a716f
Shahid ud Din Khalil <khaani@Shahid-ud-Din-Khalils-MacBook-Pro.local>
2023-07-18 23:52:08 +0100

    Added app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-# entry point
+from flask import Flask
+from flask_cors import CORS
+from book_routes import book_blueprint
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(book_blueprint, url_prefix='/bookreview')
+
+if __name__ == '__main__':
+    app.run()
